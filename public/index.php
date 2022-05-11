96e1d5ad605361e8f2ccfec31661435eb9fe9dfa
i.filippov <vanill228@yandex.ru>
2022-05-11 17:13:51 +0300

    delete tests, response sender

--- public/index.php
+++ public/index.php
@@ -1,22 +1,21 @@
 <?php
 
-use Framework\Http\RequestFactory;
-use Framework\Http\Response;
+use Framework\Http\ResponseSender;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\HtmlResponse;
+
 
 chdir(dirname(__DIR__));
 
 require 'vendor/autoload.php';
 
-$request = RequestFactory::fromGlobals();
+$request = ServerRequestFactory::fromGlobals();
 
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-$response = (new Response('Hello, ' . $name . '!'))
+$response = (new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-developer', 'Popka');
 
-header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
-foreach ($response->getHeaders() as $name => $value) {
-    header($name . ':' . $value);
-}
-echo $response->getBody();
+$emitter = new ResponseSender();
+$emitter->send($response);
